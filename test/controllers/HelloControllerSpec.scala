b4cb326ee584853ad1171f77def4a95c75b90ede
Mutsuhiro Iwamoto <mutsuhiro6@icloud.com>
2020-12-27 23:19:16 +0900

    add test for plus

--- test/controllers/HelloControllerSpec.scala
+++ test/controllers/HelloControllerSpec.scala
@@ -20,10 +20,43 @@ class HelloControllerSpec extends PlaySpec {
     }
 
 
-    """If there is no parameter, this returns "Please give a name as a query parameter named "name"." """ in {
+    """If there is no parameter, this returns \"Please give a name as a query parameter named "name".\"""" in {
       val result: Future[Result] = controller.get(None)(FakeRequest())
       assert(status(result) === 200)
       assert(contentAsString(result) === """Please give a name as a query parameter named "name".""")
     }
+  }
+
+  "plus" should {
+    "If the both of a & b parameters, `plus` returns a + b." in {
+      val a = Some(1)
+      val b = Some(3)
+      val aplusb = "4"
+      val result = controller.plus(a,b)(FakeRequest())
+      assert(status(result) === 200)
+      assert(contentAsString(result) === aplusb)
+    }
+    """If a parameter `a` lacks, `plus` returns "Please give arguments of a and b.".""" in {
+      val a = None
+      val b = Some(4)
+      val result = controller.plus(a,b)(FakeRequest())
+      assert(status(result) === 200)
+      assert(contentAsString(result) === "Please give arguments of a and b.")
+    }
+    """If a parameter `b` lacks, `plus` returns "Please give arguments of a and b.".""" in {
+      val a = Some(5)
+      val b = None
+      val result = controller.plus(a,b)(FakeRequest())
+      assert(status(result) === 200)
+      assert(contentAsString(result) === "Please give arguments of a and b.")
+    }
+    """If the both of parameters lack, `plus` returns "Please give arguments of a and b.".""" in {
+      val a = None
+      val b = None
+      val result = controller.plus(a,b)(FakeRequest())
+      assert(status(result) === 200)
+      assert(contentAsString(result) === "Please give arguments of a and b.")
+    }
+
   }
 }
