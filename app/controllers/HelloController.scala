b6d96490439d97860b433bba27a28b5bc53cfc23
Mutsuhiro Iwamoto <mutsuhiro6@icloud.com>
2020-12-29 06:12:16 +0900

    adapt language support

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -1,18 +1,25 @@
 package controllers
 
+import play.api.i18n.{I18nSupport, Messages}
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 
 import javax.inject.{Inject, Singleton}
 
+/**
+ * 多言語対応のためにI18nSupportを実装する。
+ * conf/messages.${lang}にそれぞれメッセージのための変数を書く。
+ * conf/application.confのplay.i18n.langsリストに対応言語（${lang}）を追記しておく
+ * https://www.playframework.com/documentation/2.8.x/MessagesMigration26#I18N-API-Migration
+ */
 @Singleton
-class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
 
   def get(name: Option[String]) = Action {
     implicit request: Request[AnyContent] =>
       Ok {
         name
-          .map(s => s"Hello, $s!")
-          .getOrElse("""Please give a name as a query parameter named "name".""")
+          .map(s => Messages("hello", s))
+          .getOrElse(Messages("noQuery"))
       }
   }
 
@@ -20,7 +27,7 @@ class HelloController @Inject()(cc: ControllerComponents) extends AbstractContro
     implicit request: Request[AnyContent] =>
       Ok {
         if (a.isEmpty || b.isEmpty) {
-          "Please give arguments of a and b."
+          Messages("noQuery")
         }
         else {
           (a.get + b.get).toString
